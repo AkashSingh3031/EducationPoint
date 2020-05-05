b60a50a50decc16262811fb87e1bd978dfd8e09e
AkashSingh1245 <60146338+AkashSingh1245@users.noreply.github.com>
2020-05-05 13:34:30 +0530

    Added EducationPoint File

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from blog.models import Post, BlogComment
 
 # Register your models here.
-admin.site.register((Post, BlogComment))
+admin.site.register((BlogComment))
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('tinyInject.js',)
