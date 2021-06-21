f93ea9bab50d12e2c15fe886f1c186fb1c4f5267
rishi-org-stack <69917908+rishi-org-stack@users.noreply.github.com>
2021-06-21 20:48:26 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,21 +7,6 @@ class Post(models.Model):
     sno = models.AutoField(primary_key = True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.CharField(max_length=50)
-    slug = models.CharField(max_length=150)
-    views = models.IntegerField(default=0)
-    timeStamp = models.DateTimeField(blank=True)
-
-    def __str__(self):
-        return self.title + " by " + self.author
-
-class BlogComment(models.Model):
-    sno = models.AutoField(primary_key= True)
-    comment = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
-    timeStamp = models.DateTimeField(default=now)
 
     def __str__(self):
         return self.comment[0:13] + "..." + " by " + self.user.username 
